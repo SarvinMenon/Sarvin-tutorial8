464b5d1b6293ab192620bad7f9848f8f64ab34d2
SarvinMenon <menonsarvin707@gmail.com>
2021-01-13 03:15:28 +0800

    Up until creating button listeners

--- app/src/main/java/com/ebookfrenzy/roomdemo/ui/main/MainViewModel.java
+++ app/src/main/java/com/ebookfrenzy/roomdemo/ui/main/MainViewModel.java
@@ -1,7 +1,21 @@
 package com.ebookfrenzy.roomdemo.ui.main;
-
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import com.ebookfrenzy.roomdemo.Product;
+import com.ebookfrenzy.roomdemo.ProductRepository;
+import java.util.List;
 import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+public class MainViewModel extends AndroidViewModel
+{
+    private ProductRepository repository;
+    private LiveData<List<Product>> allProducts;
+    private MutableLiveData<List<Product>> searchResults;
+    public MainViewModel (Application application) {
+        super(application);
+        repository = new ProductRepository(application);
+        allProducts = repository.getAllProducts();
+        searchResults = repository.getSearchResults();
+    }
 }
