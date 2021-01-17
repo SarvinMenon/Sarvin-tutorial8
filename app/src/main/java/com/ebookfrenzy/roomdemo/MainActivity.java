488e38155f056157b45197e40558546438656dcb
SarvinMenon <menonsarvin707@gmail.com>
2021-01-18 01:02:55 +0800

    Tutorial 8 complete.

--- app/src/main/java/com/ebookfrenzy/roomdemo/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/roomdemo/MainActivity.java
@@ -2,7 +2,10 @@ package com.ebookfrenzy.roomdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ebookfrenzy.roomdemo.ui.main.MainFragment;
 
@@ -17,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+    }
+
+    public void helpBtn(View v){
+        String url = "https://sites.google.com/view/roomdemo/home";
+        Intent intent =new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+
     }
 }
